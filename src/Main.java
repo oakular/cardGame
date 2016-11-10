c3cf7f9c9168ea3142944b831f8ff2920b46ed59
Oakular <cal.warrilow@gmail.com>
2016-11-10 22:59:17 +0000

    renaming for SinglePlayerGame and creating interface

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 	// ----------- MAIN METHOD ------------
 	public static void main(String[] args){
-		
+
 		// create game object and start game
-		Game game = new Game();
+		SinglePlayerGame game = new SinglePlayerGame();
 		game.startGame();
 	} // end of main() method
 } // end of Main Class
