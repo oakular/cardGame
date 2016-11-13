eed534f920fb4cc9e06dfcad62e9ac39dc4eb293
oakular <callum@oakular.com>
2016-11-13 11:54:43 +0000

    javadoc comments for Attribute and Card Classes

--- src/Attribute.java
+++ src/Attribute.java
@@ -1,37 +1,54 @@
 
 /*  Author: Callum Warrilow (201068703)
- *  Class Desc: Attribute Class of Card Game that creates attribute
  *  for a Card and includes methods to get information on attribute.
  *  Date: 5/11/16
  */
 
+/**  Class Desc: Attribute Class of Card Game that creates attribute. */
 public class Attribute {
 
 	// -------- FIELDS --------
+    /** Holds name of Attribute object */
 	private final String A_NAME;
-	private final int A_NUM;
+    /** Holds reference number of Attribute object */
+	private final int A_REF_NUM;
+    /** Holds value of Attribute object */
 	private final int VAL;
 
     // ---------- CONSTRUCTOR ----------
-	public Attribute(int aNum, String aName, int val){
-		this.A_NUM = aNum;
+    /** Constructor to create Attribute object.
+     * Assigns the name, reference number and value
+     * passed as parameters to the instance.
+     */
+	public Attribute(int rNum, String aName, int val){
+		this.A_REF_NUM = rNum;
 		this.A_NAME = aName;
 		this.VAL = val;
 		}
 
-	// ---- method to return Attribute Value
+	/** Method to return Attribute value.
+     * @return The value of the attribute upon which the method
+     * is called.
+     * */
 	int getVal(){
 		return VAL;
 	} // end of getVal() method
 
-    // ---- method to return the number of the attribute
-    // in a list of attributes (not its value)
+    /** Method to return the reference number of the attribute.
+     * @return Return the reference number of the object upon which
+     * the method was called.
+     */
 	int getAttrNum(){
-		return A_NUM;
+		return A_REF_NUM;
 	}
 
-	// ---- method to override toString() to print out
-	// Attribute Values in a specific format
+	/** Method to override toString() to print out
+     * Attribute fields in a specific format.
+     * Overrides {@link Object#toString toString} method to print
+     * Attribute fields.
+     * @return The String showing the fields
+     * of the Attribute upon which the method is called.
+     */
 	@Override
 	public String toString(){
 		return ((A_NUM +1) + ". " + A_NAME + "=\t" + VAL);
