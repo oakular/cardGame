6a6a834559a0238748e19f705444643ecc34e67f
oakular <callum@oakular.com>
2016-11-13 21:33:15 +0000

    promise this is the last javadoc update

--- src/Player.java
+++ src/Player.java
@@ -11,41 +11,49 @@ import java.util.Scanner;
 
 public class Player extends LinkedList{
 
-	// -------- FIELDS --------
+    // -------- FIELDS --------
+    /** Holds the Player's name. */
     protected final String P_NAME;
+    /** Holds the Player's deck of {@link Card Cards} */
     protected Queue<Card> deck;
+    /** Holds the deckSize */
     protected int deckSize;
-    protected static int bottom = 0;
-    protected static int top;
 
     // ----- CONSTRUCTOR -----
+    /** Constructor to create Player object
+     * @param pName - The name of the Player. */
     public Player(String pName){
         this.P_NAME = pName;
 
         deck = new LinkedList<Card>();
     } // end of CONSTRUCTOR
 
+    /** Method to set the current value to the
+     * card in the player's hand.
+     * Required for the SmartComputer subclass
+     * @param current - The card currently in play
+     * for the player upon which the method is called. */
     void setCurrentCard(Card current){
 
     }
-    // ---- method to allow player to choose
-    // attribute from card
+    /** Method to allow player to choose attribute from card
+     * @return The choice of attribute by the player. */
     public int chooseAttr(){
-    	int choice;
-    	Scanner attrChoice = new Scanner(System.in);
+        int choice;
+        Scanner attrChoice = new Scanner(System.in);
 
         // --- do-while loop to ensure that the player's
         // choice is within the number range of the attributes
-    	do{
-    	System.out.print("\nChoose an Attribute No: ");
-    	choice = attrChoice.nextInt();
-    	}while(choice > 6 || choice < 1);
+        do{
+        System.out.print("\nChoose an Attribute No: ");
+        choice = attrChoice.nextInt();
+        }while(choice > 6 || choice < 1);
 
-		return choice;
+        return choice;
     } // end of chooseAttr() method
 
-    // ----- method to return player name
+    /** Method to return player name. */
     String getPName(){
-    	return P_NAME;
+        return P_NAME;
     } // end of deckIsEmpty() method
 }
